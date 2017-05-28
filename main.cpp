bc566230008bd452770a8e2ea1000fa1f82e2bd8
lorow <smykupyka@gmail.com>
2017-05-28 20:40:09 +0200

    refactor code

--- main.cpp
+++ main.cpp
@@ -3,19 +3,27 @@
 #include "CommandHandler.h"
 void showHelp()
 {
-    std::cout<<"Here's how to use this compiler : ./BFCompiler path_to_gcc path_to_bf_file \n";
+    std::cout<<"Here's how to use this compiler : ./BFCompiler -c path_to_gcc -bf path_to_bf_file \n";
     std::cout<<"if you encoutered any problems, please, consider opening an issue"<<std::endl;
 }
 
 int main(int argc, char* argv[]) {
 
-    CommandHandler cmdh;
-
-    if(cmdh.cmdOptionExists(argv, argv+argc, "-h"))
-    {
-        showHelp();
+    CommandHandler commandHandler;
+    if (argc > 1) {
+        if (commandHandler.cmdOptionExists(argv, argv + argc, "-h")) {
+            showHelp();
+        }
+        if (commandHandler.cmdOptionExists(argv, argv + argc, "-c")) {
+            auto t = commandHandler.getCmdOption(argv, argv + argc, "-c");
+            std::cout<<"path to gcc is: "<<t<<"\n";
+        }
+        if (commandHandler.cmdOptionExists(argv, argv + argc, "-bf")) {
+            auto t = commandHandler.getCmdOption(argv, argv + argc, "-bf");
+            std::cout<<"path to brainfuck file is: "<<t<<"\n";
+        }
     }
     else
-        std::cout<<"nope";
+        std::cout<<"wrong arguments"<<std::endl;
     return 0;
 }
